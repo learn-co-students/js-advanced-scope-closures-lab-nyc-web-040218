fa77d12147ba68babbf6878fd096c6a281655fc4
AustinPaley <austinpaley32@gmail.com>
2018-05-16 17:54:19 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function produceDrivingRange(blockRange){
+  return function(startBlock, endBlock){
+
+    let start = parseInt(startBlock);
+    let end = parseInt(endBlock);
+    let distance = Math.abs(end - start);
+    let difference = (blockRange - distance);
+
+    if (difference > 0){
+      return (`within range by ${difference}`)
+    }
+    else {
+      return (`${Math.abs(difference)} blocks out of range`)
+    }
+  }
+}
+
+function produceTipCalculator(percent){
+  return function (fare){
+    return fare * percent
+  }
+}
