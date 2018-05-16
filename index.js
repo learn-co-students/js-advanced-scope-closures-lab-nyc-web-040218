edb6eb9544bb2755f6bdf097b9877d332ea19757
yongnicholaskim <yongnicholaskim@gmail.com>
2018-05-16 01:14:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 function produceDrivingRange(range) {
   return function(start, end) {
-    if (range > (Number.parseInt(end) - Number.parseInt(start))) {
-      return `within range by ${(range - (Number.parseInt(end) - Number.parseInt(start)))}`; 
-    } else if (range < Number.parseInt(end) - Number.parseInt(start)) {
-      return `${((Number.parseInt(end) - Number.parseInt(start)) - range)} blocks out of range`;
-    };
+    const distance = Number.parseInt(end) - Number.parseInt(start);
+
+    if (range > distance) {
+      return `within range by ${range - distance}`;
+    } else if (range < distance) {
+      return `${distance - range} blocks out of range`;
+           };
+
+    // if (range > (Number.parseInt(end) - Number.parseInt(start))) {
+    //   return `within range by ${(range - (Number.parseInt(end) - Number.parseInt(start)))}`; 
+    // } else if (range < Number.parseInt(end) - Number.parseInt(start)) {
+    //   return `${((Number.parseInt(end) - Number.parseInt(start)) - range)} blocks out of range`;
+    // };
+
   };
-}
+};
 
 function produceTipCalculator(tipPercentage) {
   return function(fare) {
     return fare * tipPercentage;
   };
-}
+};
 
 function createDriver() {
   let DriverId = 0;
@@ -21,7 +30,7 @@ function createDriver() {
     constructor(name) {
       this.name = name;
       this.id = ++DriverId;
-    }
+    };
   };
-}
+};
 
